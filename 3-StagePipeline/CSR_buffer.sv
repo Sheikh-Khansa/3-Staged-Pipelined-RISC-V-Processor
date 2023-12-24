a4ba90e4d1589982204904b1d91d3bc6679f02f0
Sheikh-Khansa <154733111+Sheikh-Khansa@users.noreply.github.com>
2023-12-24 17:00:08 +0500

    Update CSR_buffer.sv

--- 3-StagePipeline/CSR_buffer.sv
+++ 3-StagePipeline/CSR_buffer.sv
@@ -3,7 +3,7 @@ module CSR_buffer
     input  logic        clk, 
     input  logic        rst, 
     input  logic [31:0] in, 
-	output logic [31:0] out
+    output logic [31:0] out
     
 );
 	always_ff @(posedge clk) begin
