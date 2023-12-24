4a75597499849a0792ce3243c6ea0ccd26f05640
Sheikh-Khansa <154733111+Sheikh-Khansa@users.noreply.github.com>
2023-12-24 16:58:32 +0500

    Update InstructionMemory.sv

--- 3-StagePipeline/InstructionMemory.sv
+++ 3-StagePipeline/InstructionMemory.sv
@@ -6,7 +6,7 @@ module InstructionMemory
 );
     logic [31:0] instruction_memory [31:0];    
     initial begin
-        $readmemh("inst.mem", instruction_memory);	// Put Absolute Path of the machine code
+        $readmemh("inst.mem", instruction_memory);	
     end
     assign instruction = instruction_memory[addr[31:2]];
 endmodule
