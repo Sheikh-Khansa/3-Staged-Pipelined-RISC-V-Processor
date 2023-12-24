4449a6a9124e16866795ea0ffed337666464f384
Sheikh-Khansa <154733111+Sheikh-Khansa@users.noreply.github.com>
2023-12-24 16:59:21 +0500

    Update BranchCondition.sv

--- 3-StagePipeline/BranchCondition.sv
+++ 3-StagePipeline/BranchCondition.sv
@@ -4,7 +4,7 @@ module BranchCondition
     input  logic [31:0] rs2, 
     input  logic [2:0]  br_type, 
     input  logic [6:0]  opcode,
-	output logic        br_taken
+    output logic        br_taken
     
 );
 	logic branch;
